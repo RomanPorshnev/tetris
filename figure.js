a01c3e545a18683ebdf3e599bf56ca14f7dca874
Роман <90764887+RomanPorshnev@users.noreply.github.com>
2023-09-24 14:03:41 +0300

    Update figure.js

--- figure.js
+++ figure.js
@@ -1,6 +1,8 @@
 export class Figure{
     x
     y
+    xInNextFigureWindow
+    yInNextFigureWindow
     #figureTypes = ['I', 'O', 'T', 'S', 'Z', 'J', 'L']
     #figureMatrices = {
         'I': [
@@ -57,24 +59,30 @@ export class Figure{
         this.matrix = this.#figureMatrices[this.type]
         this.color = this.#colorsOfFigures[this.type]
         this.size = this.#figureMatrices[this.type].length
+        this.#initCoordinates()
+    }
+
+    #initCoordinates(){
         if (this.size === 2){
             this.x = 5
             this.y = 1
+            this.xInNextFigureWindow = 2
+            this.yInNextFigureWindow = 2
         }
         else if (this.size === 4){
             this.x = 4
             this.y = 0
+            this.xInNextFigureWindow = 1
+            this.yInNextFigureWindow = 1
         }
         else{
             this.x = 4
             this.y = 1
+            this.xInNextFigureWindow = 1
+            this.yInNextFigureWindow = 2
         }
     }
-    /*
-    Метод возвращает тип фигуры.
-    Входные данные: [min, max) - промежуток генерации номера фигуры
-    Выходные данные: символьный тип случайной фигуры
-     */
+
     #generateNextFigure(min, max){
         const figureTypeId = Math.floor(Math.random() * (max - min)) + min
         return this.#figureTypes[figureTypeId]
